e905f5f8ebe9bc09bba0031e133f1f87b8038a10
Jamie L Coleman <jlcoleman@uk.ibm.com>
2017-09-26 15:16:17 +0100

    Change echo to cat

--- vars/microserviceBuilderPipeline.groovy
+++ vars/microserviceBuilderPipeline.groovy
@@ -231,7 +231,7 @@ def deployProject (String chartFolder, String registry, String image, String ima
   if (chartFolder != null && fileExists(chartFolder)) {
     container ('helm') {
       sh "helm init --client-only"
-      sh "echo pipeline.yaml"
+      sh "cat pipeline.yaml"
       def deployCommand = "helm upgrade --install --values pipeline.yaml"
       if (fileExists("chart/overrides.yaml")) {
         deployCommand += " --values chart/overrides.yaml"
